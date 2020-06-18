bb3ab94ac4b204197446c38d239b701a2f54d4dd
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-06-18 10:05:18 +0200

    Simplification du infinite scroll

--- GaleriePhotos/ClientApp/src/atoms/visibility.tsx
+++ GaleriePhotos/ClientApp/src/atoms/visibility.tsx
@@ -5,90 +5,44 @@ export interface VisibilityProps {
     onChange: (visible: boolean) => void;
     length: number;
 }
-function isElementInViewport(el: HTMLElement) {
-    let eap: Element | null;
-    const rect = el.getBoundingClientRect();
-    const docEl = document.documentElement;
-    const vWidth = window.innerWidth || docEl.clientWidth;
-    const vHeight = window.innerHeight || docEl.clientHeight;
-    const efp = (x: number, y: number) => document.elementFromPoint(x, y);
-    const contains = el.contains
-        ? (node: Element | null) => el.contains(node)
-        : (node: Element | null) =>
-              node && el.compareDocumentPosition(node) === 0x10;
-
-    // Return false if it's not in the viewport
-    if (
-        rect.right < 0 ||
-        rect.bottom < 0 ||
-        rect.left > vWidth ||
-        rect.top > vHeight
-    )
-        return false;
-
-    // Return true if any of its four corners are visible
-    return (
-        (eap = efp(rect.left, rect.top)) === el ||
-        contains(eap) ||
-        (eap = efp(rect.right, rect.top)) === el ||
-        contains(eap) ||
-        (eap = efp(rect.right, rect.bottom)) === el ||
-        contains(eap) ||
-        (eap = efp(rect.left, rect.bottom)) === el ||
-        contains(eap) ||
-        false
-    );
-}
 
 export function Visibility({ onChange, length }: VisibilityProps) {
     const isInViewPort = useRef(false);
     const refElement = useRef<HTMLDivElement>(null);
 
-    const checkIsInViewPort = useCallback(() => {
-        const isInViewPortNow = refElement.current
-            ? isElementInViewport(refElement.current)
-            : false;
-        if (isInViewPort.current !== isInViewPortNow) {
-            isInViewPort.current = isInViewPortNow;
-            onChange(isInViewPortNow);
-        }
-    }, [isInViewPort, onChange]);
+    const checkIsInViewPort = useCallback(
+        (
+            entries: IntersectionObserverEntry[],
+            observer: IntersectionObserver
+        ) => {
+            const entry = entries[0];
+            if (entry.isIntersecting !== isInViewPort.current) {
+                isInViewPort.current = entry.isIntersecting;
+                onChange(entry.isIntersecting);
+            }
+        },
+        [isInViewPort, onChange]
+    );
 
     useEffect(() => {
         isInViewPort.current = false;
-        checkIsInViewPort();
-    }, [checkIsInViewPort, length]);
+    }, [length]);
 
     useEffect(() => {
         const element = refElement.current;
         if (element) {
-            let ancestor = element.parentElement;
-            const ancestors = new Array<HTMLElement>();
-            while (ancestor != null && ancestor.style) {
-                if (ancestor.style.overflowY === "auto") {
-                    ancestors.push(ancestor);
-                    ancestor.addEventListener("scroll", checkIsInViewPort);
-                }
-                ancestor = ancestor.parentElement;
-            }
+            const observer = new IntersectionObserver(checkIsInViewPort, {
+                threshold: [0, 1],
+            });
 
-            checkIsInViewPort();
-            document.addEventListener("ready", checkIsInViewPort);
-            window.addEventListener("load", checkIsInViewPort);
-            window.addEventListener("scroll", checkIsInViewPort);
-            window.addEventListener("resize", checkIsInViewPort);
-
+            observer.observe(element);
             return () => {
-                for (const ancestor of ancestors) {
-                    ancestor.removeEventListener("scroll", checkIsInViewPort);
-                }
-                document.removeEventListener("ready", checkIsInViewPort);
-                window.removeEventListener("load", checkIsInViewPort);
-                window.removeEventListener("resize", checkIsInViewPort);
-                window.removeEventListener("scroll", checkIsInViewPort);
+                observer.disconnect();
             };
         }
     }, [checkIsInViewPort]);
 
-    return <div ref={refElement}>{isInViewPort ? "..." : "&nbsp;"}</div>;
+    return (
+        <div ref={refElement}>{isInViewPort.current ? "..." : "&nbsp;"}</div>
+    );
 }
