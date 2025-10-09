88851c60e2ae9c127d822cbd04c7e9192ea3d34b
Sidoine De Wispelaere <sidoine@sidoine.net>
2025-10-09 21:00:14 +0200

    fix bug with non accessible tabs

--- GaleriePhotos/ClientApp/app/(app)/gallery/[galleryId]/directory/index.tsx
+++ GaleriePhotos/ClientApp/app/(app)/gallery/[galleryId]/directory/index.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { useDirectoriesStore } from "@/stores/directories";
-import { Redirect } from "expo-router";
 import { ActivityIndicator } from "react-native";
+import { useFocusEffect, useRouter } from "expo-router";
+import { observer } from "mobx-react-lite";
 
 function DirectoriesScreen() {
   const directoriesStore = useDirectoriesStore();
   const root = directoriesStore.root;
-  if (!root) {
-    return <ActivityIndicator size="large" />;
-  }
-  return (
-    <Redirect
-      href={{
+  const router = useRouter();
+  useFocusEffect(() => {
+    if (root) {
+      router.replace({
         pathname: `/(app)/gallery/[galleryId]/directory/[directoryId]`,
         params: {
           galleryId: directoriesStore.galleryId,
           directoryId: root.id,
         },
-      }}
-    />
-  );
+      });
+    }
+  });
+  return <ActivityIndicator size="large" />;
 }
 
-export default DirectoriesScreen;
+export default observer(DirectoriesScreen);
